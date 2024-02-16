a23202e5b1eac7c298be50c2fb7770f27eded898
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-02-16 13:38:24 +0300

    Create 16.02.24 практическая 2.py

---
+++ 16.02.24 практическая 2.py
@@ -0,0 +1,36 @@
+a=int(input())
+b=int(input())
+if a>b:
+    print(a, 'больше', b)
+else:
+    print(b, 'больше', a)
+
+
+a=int(input("Сторона квадрата "))
+b=int(input("Радиус круга "))
+if a>b*2:
+    print("Круг впишется")
+else:
+    print("Круг не пришется")
+
+x=int(input())
+if x<0:
+    print('y = ', x**2)
+else:
+    print('y = ', 1/(x**2))
+
+import math
+a=int(input("Сторона квадрата"))
+b=int(input("Радиус круга"))
+d=math.sqrt(a**2)
+if d>=b:
+    print("Не впишется")
+else:
+    print('Впишется')
+  a=int(input())
+b=int(input())
+if a>b:
+    print(a, 'больше', b)
+else:
+    print(b, 'больше', a)
+
