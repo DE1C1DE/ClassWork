f5a904fb929eac4fd7778153897d2853a29c6e24
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-03-14 11:26:05 +0300

    Update Практическая 12.03.24.py

--- Практическая 12.03.24.py
+++ Практическая 12.03.24.py
@@ -21,15 +21,31 @@ def a(text):
 
 while True:
     a(input())
-  # Задание 4 недоделал
-  file = open('S:\\Группы\\ИБС215 Алгоритмизация\\III Сложные задания\\data.txt')
+  # Задание 4
+file = open('S:\\Группы\\ИБС215 Алгоритмизация\\III Сложные задания\\data.txt')
 mylist = file.readlines()
 mylist=str(mylist)
 file=mylist.split("\n")
-count=0
-for i in file:
-    if 'John' in i:
-        count+=1
-print(mylist)
-print(file)
+count=mylist.count("John")
 print(count)
+# Задание 5
+with open('S:\\Группы\\ИБС215 Алгоритмизация\\III Сложные задания\\encrypt_mess.txt') as file:
+    content=file.read()
+a={'б':'a', 'в':'б', 'г':'в', 'д':'г', 'е':'д', 'ё':'е', 'ж':'ё', 'з':'ж', 'и':'з', 'й':'и', 'к':'й', 'л':'к',
+   'м':'л', 'н':'м', 'о':'н', 'п':'о', 'р':'п', 'с':'р', 'т':'с', 'у':'т', 'ф':'у', 'х':'ф', 'ц':'х', 'ч':'ц',
+   'ш':'ч', 'щ':'ш', 'ъ':'щ', 'ы':'ъ', 'ь':'ы', 'э':'ь', 'ю':'э', 'я':'ю', 'а':'я', 'О':'1', 'Д':'2', 'Т':'3',
+   'Ч':'4', 'П':'5', 'Ш':'6', 'С':'7', 'В':'8', 'Е':'9', 'Я':'0',
+   '/':'.', '_':'!', ',':' ', ' ':',', '^':':', '*':'\n'}
+for i in content:
+    if i in a:
+        print(a.get(i),end='')
+# Задание 6
+inputTXT=input("Текст> ")
+Save=input("Сохранить?> ")
+if Save== 'yes':
+    inputFileName=input('Название файла> ')
+    file = open(inputFileName, "w")
+    file.write(inputTXT)
+    file.close()
+else:
+    print("no save")
