fc12e13c5c37fab45980173c1fb4b2a919b1be1f
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-04-02 09:52:28 +0300

    Update Практическая 02.04.24.py

--- Практическая 02.04.24.py
+++ Практическая 02.04.24.py
@@ -24,3 +24,51 @@ for i in matrix:
     Result=0
             
 print(a)
+
+#37
+import random  
+def creatArray():
+    r = 0
+    print('Input first index matrix: ')
+    x = int(input())
+    print('Input second index matrix: ')
+    y = int(input())
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-1,1))
+            r += 1  
+    return array
+matrix=creatArray()
+print(matrix)
+a=[]
+for i in matrix:
+    for e in i:
+        if e != 0:
+            a.append(e)
+            
+print(a)
+
+#39
+import random  
+def creatArray():
+    r = 0
+    print('Input first index matrix: ')
+    x = int(input())
+    print('Input second index matrix: ')
+    y = int(input())
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-100,100))
+            r += 1  
+    return array
+matrix=creatArray()
+print(matrix)
+a=[]
+Min=int()
+for i in matrix:
+    Min=min(i)
+    a.append(Min)
