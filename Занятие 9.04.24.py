9b4c962a6bec09dec749ec16e6d4ca507699a5b5
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-04-09 09:59:38 +0300

    Update Занятие 9.04.24.py

--- Занятие 9.04.24.py
+++ Занятие 9.04.24.py
@@ -10,3 +10,32 @@ c, d=func()
 e, f=func()
 z, x=func()
 
+#Задача 1 и 2
+
+def Func1(a,b,c):
+    d=a+b+c   
+    print(d)    
+def Func2(a,b,c):
+    d=a-b-c
+    print(d)
+print("Что нужно сделать?")
+print("1 - Сложить, 2 - Вычесть")
+var=int(input())
+if var == 1:
+    print("Введите число 1 >>> ")
+    a=int(input())
+    print("Введите число 2 >>> ")
+    b=int(input())
+    print("Введите число 3 >>> ")
+    c=int(input())
+    Func1(a, b, c)
+elif var == 2:
+    print("Введите число 1 >>> ")
+    a=int(input())
+    print("Введите число 2 >>> ")
+    b=int(input())
+    print("Введите число 3 >>> ")
+    c=int(input())
+    Func2(a, b, c)
+else:
+    print("Введено неверное действие")
