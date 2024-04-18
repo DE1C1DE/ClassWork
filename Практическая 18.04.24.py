29a19942ba7f183b70c7484d4b51501181f783ba
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-04-18 11:32:11 +0300

    Update Практическая 18.04.24.py

--- Практическая 18.04.24.py
+++ Практическая 18.04.24.py
@@ -23,3 +23,134 @@ for i in matrix:
 print(matrix)
 print(result)
 №36
+import random
+print('Input first index matrix: ')
+x = int(input())
+print('Input second index matrix: ')
+y = int(input())
+def creatArray():
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-10,10))
+    return array
+matrix=creatArray()
+result=[]
+a=[]
+b=0
+for i in range(y):
+    for e in range(x):
+        r = matrix[e][b]
+        a.append(r)
+    average = sum(a) / len(a)
+    result.append(average)
+    average=0
+    a = []
+    b+=1
+print(matrix)
+print(result)
+#38
+import random
+print('Input first index matrix: ')
+x = int(input())
+print('Input second index matrix: ')
+y = int(input())
+def creatArray():
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-10,10))
+    return array
+matrix=creatArray()
+result=[]
+c=1
+a=[]
+b=0
+for i in range(y):
+    for e in range(x):
+        r = matrix[e][b]
+        if r<0:
+            c*=r
+    a.append(c)
+    c=1
+    b+=1
+print(matrix)
+print(a)
+#37
+import random
+print('Input first index matrix: ')
+x = int(input())
+print('Input second index matrix: ')
+y = int(input())
+def creatArray():
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-10,10))
+    return array
+matrix=creatArray()
+result=[]
+c=1
+a=[]
+b=0
+for i in range(y):
+    for e in range(x):
+        r = matrix[e][b]
+        if r<0:
+            c*=r
+    a.append(c)
+    c=1
+    b+=1
+print(matrix)
+print(a)
+#39
+import random
+print('Input first index matrix: ')
+x = int(input())
+print('Input second index matrix: ')
+y = int(input())
+def creatArray():
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-10,10))
+    return array
+matrix=creatArray()
+result=[]
+a=[]
+for i in matrix:
+    for e in i:
+        a.append(e)
+    result.append(min(a))
+    a=[]
+print(matrix)
+print(result)
+#40
+import random
+print('Input first index matrix: ')
+x = int(input())
+print('Input second index matrix: ')
+y = int(input())
+def creatArray():
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-10,10))
+    return array
+matrix=creatArray()
+result=[]
+a=0
+for i in matrix:
+    for e in i:
+        if e!=0:
+            a+=1
+    result.append(a)
+    a=0
+print(matrix)
+print(result)
+
