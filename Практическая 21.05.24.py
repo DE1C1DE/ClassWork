b22ae77c599d5400ceebda036c8ab3ead346df25
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-05-21 10:30:26 +0300

    Update Практическая 21.05.24.py

--- Практическая 21.05.24.py
+++ Практическая 21.05.24.py
@@ -1,3 +1,4 @@
+#Задание 1
 def sum1(start, end):
     if start >end:
         start, end = end, start
@@ -6,3 +7,26 @@ def sum1(start, end):
         result += i
     print(result)
 sum1(2, 8)
+#Задание 2
+функция 1: 4
+Функция 2: 4
+Функция 3: Оштбка
+#Задание 3
+a, b, c= 1, 2, 3
+def three_args(a, b, c):
+    print(a, b, c)
+three_args(a, b, c)
+Задание 4:
+Не вызывается метод. Что бы работало надо вместо
+dt
+написать
+dt.now().time()
+#Задание 5
+import inspect
+def pr(a, b, c):
+    a=2
+    print(a)
+def insp():
+    param_names = pr.__code__.co_varnames[:pr.__code__.co_argcount]
+    print(param_names)
+insp()
